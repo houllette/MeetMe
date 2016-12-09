874a5376e5f8058b549cab1d8675eb72880e7513
Holden Oullette <holdenoullette@gmail.com>
2016-12-08 19:35:01 -0800

    added more framework for nosetest

--- test_process.py
+++ test_process.py
@@ -1,1 +1,16 @@
 from process import *
+
+def test_combine():
+    assert(1==1)
+
+def test_sort_busytimes():
+    assert(1==1)
+
+def test_conflicting_events():
+    assert(1==1)
+
+def test_condense():
+    assert(1==1)
+
+def test_freetime():
+    assert(1==1)
